70440b06f847f542d133b882ace7db6ef5d7a1d1
jo <egojappensoon@gmail.com>
2021-09-19 12:55:10 +0300

    2nd demo test...

---
+++ src/test/java/RestAssuredTests/Demo2PostRequest.java
@@ -0,0 +1,19 @@
+package RestAssuredTests;
+
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+/*
+* Given() - set cookies, add auth, add param, set headers info, etc...
+* When() - get, post, put, delete,...
+* Then() - validate status code, extract response, extract headers cookies and response body, ...
+* */
+public class Demo2PostRequest {
+
+
+
+
+}
