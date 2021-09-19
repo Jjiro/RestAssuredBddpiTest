51d7a4533783b1c4c04698784d1b7d3d79a34c22
jo <egojappensoon@gmail.com>
2021-09-19 13:44:03 +0300

    Completed second test case...

--- src/test/java/RestAssuredTests/Demo2PostRequest.java
+++ src/test/java/RestAssuredTests/Demo2PostRequest.java
@@ -1,8 +1,11 @@
 package RestAssuredTests;
 
 
+import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,16 +14,39 @@ import static org.hamcrest.Matchers.equalTo;
 * Given() - set cookies, add auth, add param, set headers info, etc...
 * When() - get, post, put, delete,...
 * Then() - validate status code, extract response, extract headers cookies and response body, ...
-* */
+*
+*/
 public class Demo2PostRequest {
+
+    public static HashMap map = new HashMap();
 
     @BeforeClass
     public void postData(){
+        map.put("FirstName", RestUtils.getFirstName());
+        map.put("FirstName", RestUtils.getLastName());
+        map.put("FirstName", RestUtils.getUserName());
+        map.put("FirstName", RestUtils.getPassword());
+        map.put("FirstName", RestUtils.getEmail());
 
+        RestAssured.baseURI = "https://reqres.in/api";
+        RestAssured.basePath = "/register";
     }
 
     @Test
     public void testPostData(){
+        given()
+              .contentType("application/json")
+              .body(map)
+
+        .when()
+              .post()
+
+        .then()
+              .statusCode(201)
+              .and()
+              .body("SuccessCode",equalTo("OPERATION_SUCCESS"))
+              .and()
+              .body("Massage", equalTo("Operation completed successfully"));
 
     }
 }
--- src/test/java/RestAssuredTests/RestUtils.java
+++ src/test/java/RestAssuredTests/RestUtils.java
@@ -1,5 +1,48 @@
 package RestAssuredTests;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class RestUtils {
+
+    public static String getFirstName(){
+        String generatedString = RandomStringUtils.randomAlphabetic(1);
+        return ("John" + generatedString);
+    }
+
+    public static String getLastName(){
+        String generatedString = RandomStringUtils.randomAlphabetic(1);
+        return ("Oswald" + generatedString);
+    }
+
+    public static String getUserName(){
+        String generatedString = RandomStringUtils.randomAlphabetic(3);
+        return (generatedString+"KO");
+    }
+
+    public static String getPassword(){
+        String generatedString = RandomStringUtils.randomAlphabetic(3);
+        return ("John" + generatedString);
+    }
+
+    public static String getEmail(){
+        String generatedString = RandomStringUtils.randomAlphabetic(5);
+        return (generatedString+"@mymail.com");
+    }
+
+    public static String empName(){
+        String generatedString = RandomStringUtils.randomAlphabetic(1);
+        return ("John" + generatedString);
+    }
+
+    public static String emplSal(){
+        String generatedNumber = RandomStringUtils.randomNumeric(5);
+        return (generatedNumber);
+    }
+
+    public static String empAge(){
+        String generatedNumber = RandomStringUtils.randomNumeric(2);
+        return (generatedNumber);
+    }
+
 
 }
